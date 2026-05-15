1eaaed1fc658688bbd925c5b9388dde6f61e0a1e
Adrian Abellanosa <adrianjrfabian@gmail.com>
2026-05-15 22:42:42 +0800

    Remove invalid exclude from workbox config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -41,7 +41,6 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB to allow large images
-        exclude: [/images\/.*\.png$/], // Exclude large images from precache
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
